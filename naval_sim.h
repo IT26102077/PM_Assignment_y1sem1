762622e017694e70c3b6d8ec95907908358b6dca
IT26102077 <it26102077@my.sliit.lk>
2026-08-28 06:10:57 +0530

    Update naval_sim.h
    part1bupdated

--- naval_sim.h
+++ naval_sim.h
@@ -5,11 +5,11 @@
 
 /* ---------- Physical / simulation constants ---------- */
 #define GRAVITY 9.81
-#define PI 3.14
+#define PI 3.14159265358979323846
 #define MAX_ESCORT_SHIPS 200   /* upper bound for array sizing */
 #define NUM_ESCORT_TYPES 5
 
-//Escort ship types
+/* ---------- Escort ship types ---------- */
 typedef enum {
     ESCORT_A = 0,  /* 1936A-class Destroyer   */
     ESCORT_B = 1,  /* Gabbiano-class Corvette */
@@ -18,7 +18,7 @@ typedef enum {
     ESCORT_E = 4   /* Japanese Kaibokan       */
 } EscortTypeId;
 
-// Battleship types
+/* ---------- Battleship types ---------- */
 typedef enum {
     BATTLESHIP_U = 0, /* USS Iowa (BB-61)   */
     BATTLESHIP_M = 1, /* MS King George V   */
@@ -77,6 +77,19 @@ typedef struct {
     double flightTimeSec;
 } HitResult;
 
+/* A single (x,y) waypoint on the battleship's movement path (Part 1-B) */
+typedef struct {
+    double x, y;
+} Point;
+
+/* Summary of one battle iteration (Part 1-B: one point along B's path) */
+typedef struct {
+    int    escortsHitByB;      /* how many E's B destroyed this iteration */
+    int    battleshipSunk;     /* 1 if B was sunk this iteration          */
+    int    killerIndex;        /* index of the E that sank B, or -1       */
+    double killerFlightTime;   /* flight time of the killing shot         */
+} IterationResult;
+
 /* The whole battlefield / scenario state */
 typedef struct {
     double D;                 /* canvas is a D x D square, origin (0,0) */
@@ -86,16 +99,16 @@ typedef struct {
     EscortShip escorts[MAX_ESCORT_SHIPS];
 } Battlefield;
 
-// function prototypes for ship_data.c
+/* ---------- ship_data.c ---------- */
 const EscortTypeInfo    *get_escort_type_info(EscortTypeId t);
 const BattleshipTypeInfo *get_battleship_type_info(BattleshipTypeId t);
 EscortTypeId escort_notation_to_id(char notation);
 BattleshipTypeId battleship_notation_to_id(char notation);
 
-// function prototypes for setup.c
+/* ---------- setup.c ---------- */
 void setup_battlefield(Battlefield *bf);
 
-// function prototypes for physics.c
+/* ---------- physics.c ---------- */
 double deg2rad(double deg);
 double distance_between(double x1, double y1, double x2, double y2);
 double projectile_range(double speed, double angleDeg);
@@ -103,14 +116,23 @@ double projectile_flight_time(double speed, double angleDeg);
 double battleship_max_range(const Battlefield *bf);
 void   escort_attack_range(const Battlefield *bf, const EscortShip *e,
                             double *rMin, double *rMax);
-                            
 HitResult resolve_battleship_shot(const Battlefield *bf, const EscortShip *e);
+HitResult resolve_battleship_shot_ex(const Battlefield *bf, const EscortShip *e,
+                                      double thetaMinDeg, double thetaMaxDeg);
 HitResult resolve_escort_shot(const Battlefield *bf, const EscortShip *e);
 
-// function prototypes for simulate.c
-void run_part1a_simulation(Battlefield *bf, const char *outFilePrefix);
+/* ---------- path.c ---------- */
+void generate_random_path(const Battlefield *bf, Point *path, int k);
 
-// function prototypes for fileio.c
+/* ---------- simulate.c ---------- */
+void reset_escort_states(Battlefield *bf);
+IterationResult run_battle_iteration(Battlefield *bf, FILE *logFile, int iterationNum,
+                                      double bThetaMinDeg, double bThetaMaxDeg);
+void run_part1a_simulation(Battlefield *bf, const char *outFilePrefix);
+void run_part1b_both_simulations(Battlefield *bf, int k, int t, double jamThetaMinDeg,
+                                  const char *outFilePrefix);
+
+/* ---------- fileio.c ---------- */
 void save_initial_conditions(const Battlefield *bf, const char *filename);
 void save_final_conditions(const Battlefield *bf, const char *filename,
                             int sunkByEscortIndex);
